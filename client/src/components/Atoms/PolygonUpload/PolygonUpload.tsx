0e61192ed771b313b0710babd3e5c95b8f95a271
Roujon2 <jonroulet@gmail.com>
2024-09-29 20:55:30 -0300

    Added polygon upload functionality for kmz and kml files. Code is a bit janky and many errors can occur in the process so will have to carefully go through and fix them.

--- client/src/components/Atoms/PolygonUpload/PolygonUpload.tsx
+++ client/src/components/Atoms/PolygonUpload/PolygonUpload.tsx
@@ -3,10 +3,13 @@ import JSZip from "jszip";
 
 import { NewPolygon } from "../../../types/polygon";
 
+
 import './polygonUpload.css';
 import { IoClose } from "react-icons/io5";
 import { TiUpload } from "react-icons/ti";
 import { FiSave } from "react-icons/fi";
+
+import polygonApi from "../../../api/polygonApi";
 
 interface PolygonUploadProps {
     onUpload: (file: File) => void;
@@ -30,29 +33,90 @@ const PolygonUpload: React.FC<PolygonUploadProps> = ({ onUpload, onClose }) => {
     const [ownership_type, setOwnershipType] = React.useState<string>("");
     const [notes, setNotes] = React.useState<string>("");
     const [farm_series_name, setSeriesName] = React.useState<string>("");
+    const [coordinates, setCoordinates] = React.useState<google.maps.LatLngLiteral[]>();
+
+
+    // Function to process kml content and extract coordinates
+    const processKML = (kml: string) => {
+        try{
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(kml, "text/xml");
+
+            const coordinates = xmlDoc.getElementsByTagName("coordinates")[0].textContent;
+
+            // If there are no coordinates, return
+            if(!coordinates) {
+                throw new Error("No coordinates found in KML file");
+            }
+
+            // Split the coordinates and parse them
+            const coords = coordinates.split(" ");
+            const parsedCoords = coords.map((coord) => {
+                const [lng, lat] = coord.split(",");
+                const parsedLng = parseFloat(lng);
+                const parsedLat = parseFloat(lat);
+
+                // Check if the coordinates are valid
+                if(isNaN(parsedLat) || isNaN(parsedLng)) {
+                    return null;
+                }
+
+                return { lat: parsedLat, lng: parsedLng };
+            }).filter((coord) => coord !== null) as google.maps.LatLngLiteral[];
+
+            setCoordinates(parsedCoords);
+        }catch(err){
+            console.error(err);
+        }
+    };
 
     // If the file is changed
-    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
         setLoading(true);
         // Get the files
         const files = e.target.files;
 
         if(files) {
-            // Set the file to the first file
-            setFile(files[0]);
-            // Load the zip file into a JSZip object
-            const zip = new JSZip();
-            zip.loadAsync(files[0]).then((zip) => {
-                setZip(zip);
-                setLoading(false);
-                setShowFileDetails(true);
-            });
+            // Set the file to the first file\
+            const file = files[0];
+            setFile(file);
+
+            // Check if file is kmz or kml
+            if(file.name.endsWith(".kmz")) {
+                // Unzip and extract kml
+                const zip = new JSZip();
+                const unzipped = await zip.loadAsync(file);
+                const kml = Object.keys(unzipped.files).find((key) => key.endsWith(".kml"));
+
+                if(kml) {
+                    const kmlContent = await unzipped.files[kml].async("text");
+                    // Process kml and save coordinates
+                    processKML(kmlContent);
+                }
+            }else if(file.name.endsWith(".kml")) {
+                // Handle kml file
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    const content = e.target?.result as string;
+                    processKML(content);
+                };
+                reader.readAsText(file);
+            }
+
+            setLoading(false);
+            setShowFileDetails(true);
         }
     }
 
     // Function to handle save
     const handleSave = (e: React.FormEvent) => {
         e.preventDefault();
+
+        // If there are no coordinates, return
+        if(!coordinates) {
+            alert ("No coordinates found in KML file");
+            return;
+        }
 
         // Create polygon object
         const NewPolygon: NewPolygon = {
@@ -62,10 +126,14 @@ const PolygonUpload: React.FC<PolygonUploadProps> = ({ onUpload, onClose }) => {
             ownership_type: ownership_type,
             notes: notes,
             farm_series_name: farm_series_name,
-            coordinates: [],
+            coordinates: coordinates,
             classified: false,
         };
 
+        console.log(NewPolygon);
+
+        // Save new polygon to backend
+        polygonApi.savePolygon(NewPolygon);
     }
 
     // Function to handle upload
@@ -85,7 +153,7 @@ const PolygonUpload: React.FC<PolygonUploadProps> = ({ onUpload, onClose }) => {
                 <IoClose className="close-button" onClick={onClose}/>
                 <h1>Upload KMZ</h1>
                 <div className="polygon-upload__buttons">
-                    <input type="file" onChange={handleFileChange} />
+                    <input type="file" onChange={handleFileChange} accept=".kml, .kmz"/>
                     <button className={file ? "upload_button" : "upload_button_disabled"} onClick={handleUpload}><TiUpload /></button>
                 </div>
 
