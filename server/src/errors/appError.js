94d22ba5d9228e5d2cdd5ec2a8700158eb78b242
Roujon2 <jonroulet@gmail.com>
2024-11-22 20:30:10 -0300

    Removed error stack trace from appError.js

--- server/src/errors/appError.js
+++ server/src/errors/appError.js
@@ -4,6 +4,7 @@ class AppError extends Error {
         super(message);
         this.statusCode = statusCode;
         this.extra = extra;
+        //Error.captureStackTrace(this, this.constructor);
     }
 
     toJSON() {
