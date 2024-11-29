7fa549e4af1a3347454aa2e9a60601073aec0548
Roujon2 <jonroulet@gmail.com>
2024-11-29 08:42:06 -0300

    Rate limiter on server

--- server/server.js
+++ server/server.js
@@ -20,6 +20,15 @@ import {logError, logWarn} from './logger.js';
 
 import jwt from 'jsonwebtoken';
 
+// Rate limiter
+import rateLimit from 'express-rate-limit';
+
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100,
+    message: 'Too many requests from this IP, please try again after 15 minutes'
+});
+
 
 const app = express();
 const PORT = config.server.port;
